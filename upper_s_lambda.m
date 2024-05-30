43dcdfc4b5cb87353019d3d08690ab593c23f5a8
Kevin <kmazza2@uic.edu>
2024-05-30 11:25:47 -0500

    Added S_lambda

---
+++ upper_s_lambda.m
@@ -0,0 +1,27 @@
+function val = upper_s_lambda(Z, kernel, lambda)
+%UPPER_S_LAMBDA Returns (uppercase) S_lambda(Z)
+
+sz = size(Z);
+num_params = sz(1);
+num_obs = sz(2);
+
+% preconditions:
+assert(all(lambda >= 0));
+assert(sz(1) == length(lambda));
+
+% implementation:
+
+val = zero(num_obs, num_obs);
+
+for i = 1:num_obs
+    row = lower_s_lambda(i, Z, kernel, lambda);
+    for j = 1:num_obs
+        val(i, j) = row(j);
+    end
+end
+
+% postconditions:
+assert(all(size(val) == [num_obs num_obs]));
+
+end
+
